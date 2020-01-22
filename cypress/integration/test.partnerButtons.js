83bbf39f133a2182088b1a64765e8976b04b9178
Tatiana <cherv.tatiana@gmail.com>
2020-01-22 21:50:12 +0300

    partner buttons test

---
+++ cypress/integration/test.partnerButtons.js
@@ -0,0 +1,16 @@
+describe('check links partner buttons', () => {
+    const partners = require('../fixtures/partners');
+
+    Object.keys(partners).forEach(function (name) {
+        it(`check ${name} is available`, () => {
+            let host = partners[name];
+            cy.request('GET', host)
+                .should((response) => {
+                    expect(response.status).to.eq(200);
+                    expect(response.headers)
+                        .to.have.property('content-type')
+                        .to.eq('text/html')
+                });
+        });
+    });
+});
