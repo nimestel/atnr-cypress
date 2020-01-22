a0ff71d7585f22fdbb28e8c06c11cef8da65261d
Tatiana <cherv.tatiana@gmail.com>
2020-01-22 19:33:04 +0300

    access main page tests

--- cypress/integration/test.openMainPage.js
+++ cypress/integration/test.openMainPage.js
@@ -1,7 +1,21 @@
-import { mainPage } from '../support/pages/MainPage';
+import {mainPage} from '../support/pages/MainPage';
 
-describe('Open main page', () => {
-    it('should visit to Athanor site', () => {
-        mainPage.open();
+describe('check access home page', () => {
+    it('should visit the site', () => {
+        cy.log('when user open website');
+        cy.visit('/');
+        cy.log('then user sees the main elements of site');
+        mainPage.verifyElements();
+    });
+
+    it('GET request for the home page', () => {
+        cy.request('GET', '/')
+            .should((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.headers)
+                    .to.have.property('content-type')
+                    .to.eq('text/html')
+                expect(response.body).contain('<title>Athanor</title>');
+            });
     });
 });
