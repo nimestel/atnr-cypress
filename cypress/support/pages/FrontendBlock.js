537f10ddd756a9e167a9469b83ece2ab37cccce4
Tatiana <cherv.tatiana@gmail.com>
2020-03-15 14:26:02 +0300

    page for frontend block

---
+++ cypress/support/pages/FrontendBlock.js
@@ -0,0 +1,22 @@
+export class FrontendBlock {
+
+    constructor() {
+        this.mainElement = '[data-anchor="expertises-frontend"]';
+        this.cardMaintenance = '.card_maintenance';
+        this.cardSpa = '.card_spa';
+        this.cardInvolvement = '.card_involvement';
+        this.cardAnyScreen = '.card_any-screen';
+        this.cardAnyPlatform = '.card_any-platform';
+    }
+
+    verifyElements() {
+        cy.get(this.mainElement).find(this.cardMaintenance).should('be.visible');
+        cy.get(this.mainElement).find(this.cardSpa).should('be.visible');
+        cy.get(this.mainElement).find(this.cardInvolvement).should('be.visible');
+        cy.get(this.mainElement).find(this.cardAnyScreen).should('be.visible');
+        cy.get(this.mainElement).find(this.cardAnyPlatform).should('be.visible');
+    }
+
+}
+
+export const frontendBlock = new FrontendBlock();
