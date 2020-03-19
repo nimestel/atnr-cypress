e4e768014dffcba9f54e3a94f51b29cc35add077
Tatiana <cherv.tatiana@gmail.com>
2020-03-19 11:06:23 +0300

    test popup menu

---
+++ cypress/integration/test.openByPopupMenu.js
@@ -0,0 +1,33 @@
+import {mainPage} from '../support/pages/MainPage';
+import {popupMenu} from '../support/pages/PopupMenu';
+import {navMenu} from "../support/pages/NavigationMenu";
+
+describe('Scroll to main blocks by popup menu', () => {
+    beforeEach(() => {
+        // popup menu appears when we scroll to How We Work block
+        mainPage.open()
+            cy.then(() => {
+                    navMenu.openHowWeWorkBlock();
+            })
+    });
+
+    it('check all elements of the popup menu', () => {
+        popupMenu.verifyElements();
+    });
+
+    it('click to "how we work" link', () => {
+        popupMenu.openHowWeWorkBlock().verifyElements();
+    });
+
+    it('click to "expertise" link', () => {
+        popupMenu.openExpertiseBlock().verifyElements();
+    });
+
+    it('click to "what we do" link', () => {
+        popupMenu.openWhatWeDoBlock().verifyElements();
+    });
+
+    it('click to "agency" link', () => {
+        popupMenu.openAgencyBlock().verifyElements();
+    });
+});
---
+++ cypress/support/pages/PopupMenu.js
@@ -0,0 +1,46 @@
+import {hwwBlock1} from './HowWeWorkBlock1';
+import {wwdBlock} from './WhatWeDoBlock';
+import {agencyBlock} from './AgencyBlock';
+import {designBlock} from "./DesignBlock";
+
+export class PopupMenu {
+
+
+    constructor() {
+        this.mainElement = '.navigation__items';
+        this.linkHowWeWork = '.navigation-link:nth-child(1)';
+        this.linkExpertise = '.navigation-link:nth-child(2)';
+        this.linkWhatWeDo = '.navigation-link:nth-child(3)';
+        this.linkAgency = '.navigation-link:nth-child(4)';
+    }
+
+    verifyElements() {
+        cy.get(this.mainElement).find(this.linkHowWeWork).should('be.visible');
+        cy.get(this.mainElement).find(this.linkExpertise).should('be.visible');
+        cy.get(this.mainElement).find(this.linkWhatWeDo).should('be.visible');
+        cy.get(this.mainElement).find(this.linkAgency).should('be.visible');
+    }
+
+    openHowWeWorkBlock() {
+        cy.get(this.mainElement).find(this.linkHowWeWork).click();
+        return hwwBlock1;
+    }
+
+    openExpertiseBlock() {
+        cy.get(this.mainElement).find(this.linkExpertise).click();
+        return designBlock;
+    }
+
+    openWhatWeDoBlock() {
+        cy.get(this.mainElement).find(this.linkWhatWeDo).click();
+        return wwdBlock;
+    }
+
+    openAgencyBlock() {
+        cy.get(this.mainElement).find(this.linkAgency).click();
+        return agencyBlock;
+    }
+
+}
+
+export const popupMenu = new PopupMenu();
